3d84bff331fc1d7aa3cc6d028cca379dd654b6ff
Travis Williamson <travis.m.williamson@gmail.com>
2024-12-07 10:11:28 -0600

    Day 6 part 2

--- 2024-rust/src/solutions/day06.rs
+++ 2024-rust/src/solutions/day06.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, error::Error};
 
-use itertools::Position;
+use rayon::prelude::*;
 
 pub fn first(_input: String) -> Result<String, Box<dyn Error>> {
     let mut grid: HashMap<(i32, i32), char> = _input.split("\n").enumerate().map(|l| l.1.chars().enumerate().map(move |c| ((l.0 as i32, c.0 as i32), c.1))).flatten().collect();
@@ -28,12 +28,14 @@ pub fn first(_input: String) -> Result<String, Box<dyn Error>> {
 pub fn second(_input: String) -> Result<String, Box<dyn Error>> {
     let mut grid: HashMap<(i32, i32), (char, Option<(i32,i32)>, bool)> = _input.split("\n").enumerate().map(|l| l.1.chars().enumerate().map(move |c| ((l.0 as i32, c.0 as i32), (c.1, None, true)))).flatten().collect();
     let mut position = *grid.iter().find(|e| e.1.0 == '^').unwrap().0;
+    let start_position = position.clone();
     grid.get_mut(&position).unwrap().2 = false;
     let mut direction = (-1,0);
     let right_turns = [(-1,0), (0,1), (1,0), (0,-1)];
     while grid.contains_key(&position) {
-        grid.get_mut(&position).unwrap().0 = 'X';
-        grid.get_mut(&position).unwrap().1 = Some(direction);
+        let current = grid.get_mut(&position).unwrap();
+        current.0 = 'X';
+        current.1 = Some(direction);
         let next = (position.0 + direction.0, position.1 + direction.1);
         if grid.contains_key(&next) {
             if grid.get(&next).unwrap().0 == '#' {
@@ -47,6 +49,40 @@ pub fn second(_input: String) -> Result<String, Box<dyn Error>> {
             position = next;
         }
     }
-    grid.iter().filter(|e| e.1.0 == 'X' && e.1.2).map(|e| e.0).collect();
-    Ok(grid.into_values().filter(|c| c.0 == 'X').count().to_string())
+    let spots_to_check: Vec<(&(i32, i32), &(char, Option<(i32,i32)>, bool))> = grid.iter().filter(|c| c.1.0 == 'X' && c.1.2).collect();
+    Ok(spots_to_check
+        .par_iter()
+        .filter(|s| {
+            let mut thread_grid = grid.clone();
+            let mut thread_position = start_position.clone();
+            let mut thread_direction = (-1,0);
+            for entry in thread_grid.values_mut() {
+                entry.1 = None;
+            }
+            thread_grid.get_mut(&s.0).unwrap().0 = '#';
+            while thread_grid.contains_key(&thread_position) {
+                let thread_current = thread_grid.get_mut(&thread_position).unwrap();
+                if thread_current.0 == 'X' && thread_current.1 == Some(thread_direction) {
+                    return true;
+                }
+                thread_current.0 = 'X';
+                if thread_current.1 == None {
+                    thread_current.1 = Some(thread_direction);
+                }
+                let thread_next = (thread_position.0 + thread_direction.0, thread_position.1 + thread_direction.1);
+                if thread_grid.contains_key(&thread_next) {
+                    if thread_grid.get(&thread_next).unwrap().0 == '#' {
+                        thread_direction = right_turns[(right_turns.iter().position(|d| *d == thread_direction).unwrap() + 1) % 4];
+                    }
+                    else {
+                        thread_position = thread_next;
+                    }
+                }
+                else {
+                    thread_position = thread_next;
+                }
+            }
+            false
+        })
+        .count().to_string())
 }
