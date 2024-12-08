f7f7eb3a0c513d5a7f8171393f43b87ba1c9517c
Travis Williamson <travis.m.williamson@gmail.com>
2024-12-07 20:09:19 -0600

    Refactor day 4 to be multithreaded

--- 2024-rust/src/solutions/day04.rs
+++ 2024-rust/src/solutions/day04.rs
@@ -1,28 +1,32 @@
 use std::error::Error;
 use itertools::Itertools;
+use rayon::prelude::*;
 
 pub fn first(_input: String) -> Result<String, Box<dyn Error>> {
     let grid : Vec<Vec<char>> = _input.split("\n").map(|s| s.chars().collect()).collect();
     let grid_height = 0..(grid.len() as i32);
     let grid_len = 0..(grid[0].len() as i32);
     let directions: Vec<(i32, i32)> = (-1..2).cartesian_product(-1..2).collect();
-    let mut count = 0;
-    for (i, j) in grid_height.clone().cartesian_product(grid_len.clone()) {
-        for direction in directions.iter() {
-            let mut word = String::from("");
-            for index in 0..4 {
-                let i : i32 = i + (direction.0 * index);
-                let j : i32 = j + (direction.1 * index);
-                if grid_height.contains(&i) && grid_len.contains(&j) {
-                    word.push(grid[i as usize][j as usize]);
+    let spots: Vec<(i32, i32)> = grid_height.clone().cartesian_product(grid_len.clone()).collect();
+    Ok(spots.par_iter()
+        .map(|(i, j)| {
+            let mut count = 0;
+            for direction in directions.iter() {
+                let mut word = String::from("");
+                for index in 0..4 {
+                    let i : i32 = i + (direction.0 * index);
+                    let j : i32 = j + (direction.1 * index);
+                    if grid_height.contains(&i) && grid_len.contains(&j) {
+                        word.push(grid[i as usize][j as usize]);
+                    }
+                }
+                if word == "XMAS" {
+                    count += 1;
                 }
             }
-            if word == "XMAS" {
-                count += 1;
-            }
-        }
-    }
-    return Ok(count.to_string())
+            count
+        })
+        .sum::<i32>().to_string())
 }
 
 pub fn second(_input: String) -> Result<String, Box<dyn Error>> {
