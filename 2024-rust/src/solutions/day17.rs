9a438c70e6a9f299b01c6b92382226eed4ef2aac
Travis Williamson <twilliamson@capsher.com>
2024-12-21 14:59:01 -0600

    Day 21

--- 2024-rust/src/solutions/day17.rs
+++ 2024-rust/src/solutions/day17.rs
@@ -52,70 +52,65 @@ pub fn first(_input: String) -> Result<String, Box<dyn Error>> {
     Ok(output.iter().map(|u| u.to_string()).join(","))
 }
 
-static mut PROGRAM: Vec<u64> = vec![];
-
-fn find_a_for_output(index_of_output_to_sieve: i32, a_to_check: u64) -> Option<u64> {
+fn find_a_for_output(index_of_output_to_sieve: i32, a_to_check: u64, program: &Vec<u64>) -> Option<u64> {
     if index_of_output_to_sieve < 0 {
         // We done!
         Some(a_to_check)
     }
     else {
         for low_digits in 0..8 {
-            // Trust me, it's fine
-            unsafe {
-                let mut a = (a_to_check << 3) | low_digits;
-                // Turns out, these are reset every loop
-                let mut b = 0_u64;
-                let mut c = 0_u64;
-                let mut instruction_pointer = 0;
-                let mut output = u64::MAX;
+            let mut a = (a_to_check << 3) | low_digits;
+            // Turns out, these are reset every loop
+            let mut b = 0_u64;
+            let mut c = 0_u64;
+            let mut instruction_pointer = 0;
+            let mut output = u64::MAX;
 
-                while instruction_pointer < PROGRAM.len() {
-                    let command = PROGRAM[instruction_pointer];
-                    let literal_operand = PROGRAM[instruction_pointer + 1];
-                    let combo_operand = match literal_operand {
-                        0 | 1 | 2 | 3 => literal_operand,
-                        4 => a,
-                        5 => b,
-                        6 => c,
-                        7 => u64::MAX,
-                        _ => panic!("Bad input of {:?}", literal_operand)
-                    };
-                    // println!("{:?}", command);
-                    // println!("{:?}", literal_operand);
-                    // println!("{:?}", combo_operand);
-                    match command {
-                        // Why doesn't it have a 64 bit check shifted function?
-                        0 => a = a.checked_shr(combo_operand.try_into().unwrap()).unwrap_or(0),
-                        1 => b = b ^ literal_operand,
-                        2 => b = combo_operand & 0b111,
-                        3 => {
-                            if a > 0 {
-                                instruction_pointer = literal_operand as usize;
-                                continue;
-                            }
+            while instruction_pointer < program.len() {
+                let command = program[instruction_pointer];
+                let literal_operand = program[instruction_pointer + 1];
+                let combo_operand = match literal_operand {
+                    0 | 1 | 2 | 3 => literal_operand,
+                    4 => a,
+                    5 => b,
+                    6 => c,
+                    7 => u64::MAX,
+                    _ => panic!("Bad input of {:?}", literal_operand)
+                };
+                // println!("{:?}", command);
+                // println!("{:?}", literal_operand);
+                // println!("{:?}", combo_operand);
+                match command {
+                    // Why doesn't it have a 64 bit check shifted function?
+                    0 => a = a.checked_shr(combo_operand.try_into().unwrap()).unwrap_or(0),
+                    1 => b = b ^ literal_operand,
+                    2 => b = combo_operand & 0b111,
+                    3 => {
+                        if a > 0 {
+                            instruction_pointer = literal_operand as usize;
+                            continue;
                         }
-                        4 => b = b ^ c,
-                        5 => {
-                            // New stuff here
-                            // We're only looking for one output
-                            output = combo_operand & 0b111;
-                            break;
-                        },
-                        6 => b = a.checked_shr(combo_operand.try_into().unwrap()).unwrap_or(0),
-                        7 => c = a.checked_shr(combo_operand.try_into().unwrap()).unwrap_or(0),
-                        _ => panic!("Bad command of {:?}", command)
                     }
-                    instruction_pointer += 2;
+                    4 => b = b ^ c,
+                    5 => {
+                        // New stuff here
+                        // We're only looking for one output
+                        output = combo_operand & 0b111;
+                        break;
+                    },
+                    6 => b = a.checked_shr(combo_operand.try_into().unwrap()).unwrap_or(0),
+                    7 => c = a.checked_shr(combo_operand.try_into().unwrap()).unwrap_or(0),
+                    _ => panic!("Bad command of {:?}", command)
                 }
+                instruction_pointer += 2;
+            }
 
-                if output == PROGRAM[index_of_output_to_sieve as usize] {
-                    let bigger_a = find_a_for_output(index_of_output_to_sieve - 1, (a_to_check << 3) | low_digits);
-                    if bigger_a.is_some() {
-                        // println!("Found output {:?} at index {:?}", output, index_of_output_to_sieve);
-                        // println!("A was {:?}, trying out {:?}", a_to_check, (a_to_check << 3) | low_digits);
-                        return bigger_a;
-                    }
+            if output == program[index_of_output_to_sieve as usize] {
+                let bigger_a = find_a_for_output(index_of_output_to_sieve - 1, (a_to_check << 3) | low_digits, &program);
+                if bigger_a.is_some() {
+                    // println!("Found output {:?} at index {:?}", output, index_of_output_to_sieve);
+                    // println!("A was {:?}, trying out {:?}", a_to_check, (a_to_check << 3) | low_digits);
+                    return bigger_a;
                 }
             }
         }
@@ -125,10 +120,8 @@ fn find_a_for_output(index_of_output_to_sieve: i32, a_to_check: u64) -> Option<u
 
 pub fn second(_input: String) -> Result<String, Box<dyn Error>> {
     // I know, trust me
-    unsafe {
-        PROGRAM = _input.trim().split("\n\n").skip(1).next().unwrap().split(": ").skip(1).next().unwrap()
-            .split(",").map(|i| i.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-        // println!("{:?}", PROGRAM);
-        Ok(find_a_for_output((PROGRAM.len() - 1) as i32, 0).unwrap().to_string())
-    }
+    let program = _input.trim().split("\n\n").skip(1).next().unwrap().split(": ").skip(1).next().unwrap()
+        .split(",").map(|i| i.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+    // println!("{:?}", program);
+    Ok(find_a_for_output((program.len() - 1) as i32, 0, &program).unwrap().to_string())
 }
--- 2024-rust/src/solutions/day21.rs
+++ 2024-rust/src/solutions/day21.rs
@@ -1,9 +1,95 @@
 use std::error::Error;
+use memoize::memoize;
+
+fn char_to_spot(c: &char, first: bool) -> (i8, i8) {
+    match c {
+        '0' => (3, 1),
+        '1' => (2, 0),
+        '2' => (2, 1),
+        '3' => (2, 2),
+        '4' => (1, 0),
+        '5' => (1, 1),
+        '6' => (1, 2),
+        '7' => (0, 0),
+        '8' => (0, 1),
+        '9' => (0, 2),
+        '<' => (1, 0),
+        'v' => (1, 1),
+        '^' => (0, 1),
+        '>' => (1, 2),
+        'A' => match first {
+            true => (3, 2),
+            false => (0, 2)
+        },
+        _ => panic!("Bad char parsing of {:?} at first: {:?}!", c, first)
+    }
+}
+
+fn diff_to_path(diff: i8, vertical: bool) -> String {
+    match (diff, vertical) {
+        (d, true) if d < 0 => str::repeat("^", diff.abs() as usize),
+        (d, true) if d > 0 => str::repeat("v", diff.abs() as usize),
+        (d, false) if d < 0 => str::repeat("<", diff.abs() as usize),
+        (d, false) if d > 0 => str::repeat(">", diff.abs() as usize),
+        (0, _) => String::from(""),
+        (_, _) => panic!("Diff of {:?} and vertical: {:?} not covered!", diff, vertical)
+    }
+}
+
+#[memoize]
+fn expand_path(path: String, depth: u16, first: bool) -> u64 {
+    let mut current_spot = match first {
+        true => (3, 2),
+        false => (0, 2)
+    };
+    let bad_spot = match first {
+        true => (3, 0),
+        false => (0, 0)
+    };
+    let mut count: u64 = 0;
+    for next_spot in path.chars().map(|c| char_to_spot(&c, first)) {
+        let vert_diff = next_spot.0 - current_spot.0;
+        let hor_diff = next_spot.1 - current_spot.1;
+        let contains_bad = (current_spot.0 == bad_spot.0 || next_spot.0 == bad_spot.0) && (current_spot.1 == bad_spot.1 || next_spot.1 == bad_spot.1);
+        let prefer_hor = (hor_diff < 0 && !contains_bad) || (hor_diff > 0 && contains_bad);
+
+        // Build path to next spot
+        let mut path = String::new();
+        if prefer_hor {
+            path.push_str(&diff_to_path(hor_diff, false));
+            path.push_str(&diff_to_path(vert_diff, true));
+        }
+        else {
+            path.push_str(&diff_to_path(vert_diff, true));
+            path.push_str(&diff_to_path(hor_diff, false));
+        }
+        path.push('A');
+
+        // Get length
+        if depth == 0 {
+            count += path.len() as u64;
+        }
+        else {
+            count += expand_path(path, depth - 1, false);
+        }
+        // if first {
+        //     println!("Found length from {:?} to {:?} using path: {:?}. Count so far: {:?}", current_spot, next_spot, path, count);
+        //     println!("Vert diff {:?}, hor diff {:?}, contains bad {:?}, prefer hor {:?}", vert_diff, hor_diff, contains_bad, prefer_hor);
+        // }
+        current_spot = next_spot;
+    }
+    // println!("{:?}", count);
+    count
+}
 
 pub fn first(_input: String) -> Result<String, Box<dyn Error>> {
-    return Err(Box::<dyn Error>::from("Not implemented!"));
+    Ok(_input.split_terminator("\n").map(|l| {
+        expand_path(l.to_string(), 2, true) * l[0..l.len() - 1].parse::<u64>().unwrap()
+    }).sum::<u64>().to_string())
 }
 
 pub fn second(_input: String) -> Result<String, Box<dyn Error>> {
-    return Err(Box::<dyn Error>::from("Not implemented!"));
+    Ok(_input.split_terminator("\n").map(|l| {
+        expand_path(l.to_string(), 25, true) * l[0..l.len() - 1].parse::<u64>().unwrap()
+    }).sum::<u64>().to_string())
 }
