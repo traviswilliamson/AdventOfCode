a29c8410591dc8a0e5a3f1f471e884c90d05f2fb
Travis Williamson <travis.m.williamson@gmail.com>
2025-10-18 16:55:06 -0500

    Fetch and parse day

--- 2025-elixir/lib/entrypoint.ex
+++ 2025-elixir/lib/entrypoint.ex
@@ -2,9 +2,12 @@ defmodule Main do
   use Application
 
   def start(_type, _args) do
-    # TODO: Get day
-    ServerIO.download_input_file(1)
+    {day, type} = File.read!("input/day.txt")
+    |> Integer.parse
+    ServerIO.download_input_file(day)
     # TODO: Run code for the day
+
+    # Normally a supervision tree. We're just exiting.
     Task.start(fn -> IO.puts IO.ANSI.cyan() <> "Done!" end)
   end
 end
