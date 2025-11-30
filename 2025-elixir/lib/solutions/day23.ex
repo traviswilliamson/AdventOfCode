f2631bb423b1f63a3dc43793bbae7cbf7db0da23
Travis Williamson <travis.m.williamson@gmail.com>
2025-11-30 16:44:14 -0600

    Remove extra days

--- 2025-elixir/lib/solutions/day23.ex
+++
@@ -1,9 +0,0 @@
-defmodule Solutions.Day23 do
-  def part_a do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
-  end
-
-  def part_b do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
-  end
-end
