ed07818e51adfd9775d2478796d051330119f774
Travis Williamson <travis.m.williamson@gmail.com>
2025-12-01 17:46:55 -0600

    Day 1, part b

--- 2025-elixir/lib/server_io.ex
+++ 2025-elixir/lib/server_io.ex
@@ -2,9 +2,9 @@ defmodule ServerIO do
   def download_input_file(day) when is_number(day) do
     unless File.exists? "input/#{day}.txt" do
       Req.get!("https://adventofcode.com/2025/day/#{day}/input", headers: %{cookie: File.read!("input/cookie.txt")}, into: File.stream!("input/#{day}.txt"))
-      IO.puts IO.ANSI.green() <> "Downloaded input" <> IO.ANSI.reset()
+      IO.puts [IO.ANSI.green(), "Downloaded input", IO.ANSI.reset()]
     else
-      IO.puts IO.ANSI.magenta() <> "Input already downloaded" <> IO.ANSI.reset()
+      IO.puts [IO.ANSI.magenta(), "Input already downloaded", IO.ANSI.reset()]
     end
   end
 end
--- 2025-elixir/lib/solutions/day01.ex
+++ 2025-elixir/lib/solutions/day01.ex
@@ -16,7 +16,35 @@ defmodule Solutions.Day01 do
     |> IO.puts
   end
 
+  def count0(0), do: 1
+  def count0(_), do: 0
+
+  def move2(line, {pos, _}) do
+    {dir, strcount} = line
+    |> String.split_at(1)
+    count = String.to_integer(strcount)
+
+    newpos = case dir do
+      "L" -> Integer.mod(pos - count, 100)
+      "R" -> Integer.mod(pos + count, 100)
+    end
+
+    newhits = case dir do
+      "L" -> div(count + newpos, 100) + count0(newpos) - count0(pos)
+      "R" -> div(count + pos, 100)
+    end
+
+    # Was useful for debugging
+    # IO.puts [IO.ANSI.white(), Integer.to_string(pos), " ", IO.ANSI.yellow(), line, " ", IO.ANSI.green(), Integer.to_string(newpos), " ", IO.ANSI.cyan(), Integer.to_string(newhits)]
+
+    {newpos, newhits}
+  end
+
   def part_b do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
+    File.stream!("input/1.txt")
+    |> Stream.map(&String.trim/1)
+    |> Stream.scan({50, 0}, &move2/2)
+    |> Enum.sum_by(fn {_, hits} -> hits end)
+    |> IO.puts
   end
 end
