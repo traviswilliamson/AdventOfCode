4e7e98b367f1f5e2db46e17411094f477abb0207
Travis Williamson <travis.m.williamson@gmail.com>
2025-12-02 12:01:20 -0600

    Add user agent header

--- 2025-elixir/lib/server_io.ex
+++ 2025-elixir/lib/server_io.ex
@@ -1,7 +1,7 @@
 defmodule ServerIO do
   def download_input_file(day) when is_number(day) do
     unless File.exists? "input/#{day}.txt" do
-      Req.get!("https://adventofcode.com/2025/day/#{day}/input", headers: %{cookie: File.read!("input/cookie.txt")}, into: File.stream!("input/#{day}.txt"))
+      Req.get!("https://adventofcode.com/2025/day/#{day}/input", headers: %{cookie: File.read!("input/cookie.txt"), user_agent: "Travis Williamson: https://github.com/traviswilliamson/AdventOfCode" }, into: File.stream!("input/#{day}.txt"))
       IO.puts [IO.ANSI.green(), "Downloaded input", IO.ANSI.reset()]
     else
       IO.puts [IO.ANSI.magenta(), "Input already downloaded", IO.ANSI.reset()]
