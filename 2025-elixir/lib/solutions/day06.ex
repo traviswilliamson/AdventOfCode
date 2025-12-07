a776edec92d65dae2e88550cf999f4125faa07d3
Travis Williamson <travis.m.williamson@gmail.com>
2025-12-07 16:35:24 -0600

    Day 6, part a

--- 2025-elixir/lib/solutions/day06.ex
+++ 2025-elixir/lib/solutions/day06.ex
@@ -1,6 +1,24 @@
 defmodule Solutions.Day06 do
+  def parse(text) do
+    case Integer.parse(text) do
+      {num, ""} -> num
+      :error -> case text do
+        "+" -> &Kernel.+/2
+        "*" -> &Kernel.*/2
+      end
+    end
+  end
+
   def part_a do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
+    (for line <- File.stream!("input/6.txt"),
+      vals = String.split(line),
+      intvals = (for val <- vals, do: parse(val)),
+      do: intvals)
+    |> Enum.zip_with(&Function.identity/1) # Transposes matrix
+    |> Stream.map(&Enum.reverse/1) # Get the operand first
+    |> Stream.map(fn [op | vals] -> Enum.reduce(vals, op) end)
+    |> Enum.sum
+    |> IO.puts
   end
 
   def part_b do
