a32f0fbc65db1c2e94558e71d0aecbb126f65164
Travis Williamson <travis.m.williamson@gmail.com>
2025-12-07 15:14:11 -0600

    Day 5

--- 2025-elixir/lib/solutions/day05.ex
+++ 2025-elixir/lib/solutions/day05.ex
@@ -1,9 +1,43 @@
 defmodule Solutions.Day05 do
   def part_a do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
+    [strranges, strvalues] = File.read!("input/5.txt")
+    |> String.split("\n\n")
+    |> Enum.map(&String.trim/1)
+
+    ranges = for line <- String.split(strranges, "\n"),
+      [start, stop] = String.split(line, "-"),
+      istart = String.to_integer(start),
+      istop = String.to_integer(stop),
+      do: istart..istop
+
+    (for strval <- String.split(strvalues, "\n"),
+      val = String.to_integer(strval),
+      Enum.any?(ranges, fn range -> val in range end),
+      do: 1)
+    |> Enum.count
+    |> IO.puts
   end
 
   def part_b do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
+    [strranges, _] = File.read!("input/5.txt")
+    |> String.split("\n\n")
+    |> Enum.map(&String.trim/1)
+
+    (for line <- String.split(strranges, "\n"),
+      [start, stop] = String.split(line, "-"),
+      istart = String.to_integer(start),
+      istop = String.to_integer(stop),
+      do: istart..istop)
+    |> Enum.sort(fn l, r -> l.first <= r.first end)
+    |> Enum.reduce([..], fn cur, [next | tail] ->
+      if Range.disjoint?(cur, next) do
+        [cur, next | tail]
+      else
+        [Range.new(min(cur.first, next.first), max(cur.last, next.last)) | tail]
+      end
+    end)
+    |> Stream.map(&(Range.size(&1)))
+    |> Enum.sum
+    |> IO.puts
   end
 end
