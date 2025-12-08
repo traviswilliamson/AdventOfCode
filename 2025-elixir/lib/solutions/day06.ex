826cab18f9a0de9022e0e1c34a17c85a2b789f25
Travis Williamson <travis.m.williamson@gmail.com>
2025-12-07 20:20:40 -0600

    Day 6, part b

--- 2025-elixir/lib/solutions/day06.ex
+++ 2025-elixir/lib/solutions/day06.ex
@@ -22,6 +22,27 @@ defmodule Solutions.Day06 do
   end
 
   def part_b do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
+    File.read!("input/6.txt")
+    |> String.split("\n")
+    |> Enum.slice(0..-2//1) # Get rid of the empty last line
+    |> Stream.map(&String.reverse/1) # We're gonna work backwards
+    |> Stream.map(&String.codepoints/1) # Grab those sweet, sweet codepoints
+    |> Stream.zip_with(&Function.identity/1) # Transposes matrix
+    |> Stream.filter(&!Enum.all?(&1, fn c -> c == " " end)) # Get rid of the gap between problems
+    |> Enum.reduce([[0]], fn row, [current | answers] ->
+      {num, _} = row |> Enum.slice(0..-2//1) |> List.to_string |> String.trim |> Integer.parse
+      problem = case current do
+        [0] -> [num] # [0] is our placeholder problem
+        _ -> [num | current]
+      end
+      case Enum.at(row, -1) do # Solve?
+        " " -> [problem | answers] # Solve later
+        "*" -> [[0], Enum.reduce(problem, &Kernel.*/2) | answers]
+        "+" -> [[0], Enum.reduce(problem, &Kernel.+/2) | answers]
+      end
+    end)
+    |> Enum.slice(1..-1//1) # Get rid of that [0] we prepended
+    |> Enum.sum
+    |> IO.puts
   end
 end
