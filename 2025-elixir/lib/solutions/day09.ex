338c481f4e3a7ca2669e84af76484448bd658643
Travis Williamson <travis.m.williamson@gmail.com>
2025-12-09 20:48:54 -0600

    Day 9, part a

--- 2025-elixir/lib/solutions/day09.ex
+++ 2025-elixir/lib/solutions/day09.ex
@@ -1,6 +1,19 @@
 defmodule Solutions.Day09 do
+  def area({{a_x, a_y}, {b_x, b_y}}) do
+    (abs(a_x - b_x) + 1) * (abs(a_y - b_y) + 1)
+  end
+
   def part_a do
-    IO.puts IO.ANSI.yellow <> "Not implemented" <> IO.ANSI.reset
+    points = for line <- File.stream!("input/9.txt"),
+      [x, y] = line |> String.trim |> String.split(","),
+      do: {String.to_integer(x), String.to_integer(y)}
+
+    (for {i, i_k} <- points |> Enum.with_index,
+      {j, j_k} <- points |> Enum.with_index,
+      i_k < j_k, do: {i, j}) # Unique pairs
+    |> Stream.map(&area/1)
+    |> Enum.max
+    |> IO.puts
   end
 
   def part_b do
