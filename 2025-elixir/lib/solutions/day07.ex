2cf3d800b07e0b28def7e7a94b58af16eea293c4
Travis Williamson <travis.m.williamson@gmail.com>
2025-12-09 18:13:02 -0600

    Day 7, more elegantly

--- 2025-elixir/lib/solutions/day07.ex
+++ 2025-elixir/lib/solutions/day07.ex
@@ -1,22 +1,22 @@
 defmodule Solutions.Day07 do
-  def beam(map, {pos_row, pos_col}) do
-    case map[{pos_row + 1, pos_col}] do
+  def beam({pos_row, pos_col}) do
+    case Process.get({pos_row + 1, pos_col}) do
       "." ->
-        map = Map.put(map, {pos_row + 1, pos_col}, "|")
-        beam(map, {pos_row + 1, pos_col})
-      nil -> map
-      "|" -> map
+        Process.put({pos_row + 1, pos_col}, "|")
+        beam({pos_row + 1, pos_col})
+      nil -> nil
+      "|" -> nil
       "^" ->
-        map = Map.put(map, {pos_row + 1, pos_col}, 1)
-        map = Map.put(map, {pos_row + 1, pos_col + 1}, "|")
-        map = Map.put(map, {pos_row + 1, pos_col - 1}, "|")
-        map = beam(map, {pos_row + 1, pos_col + 1})
-        beam(map, {pos_row + 1, pos_col - 1})
+        Process.put({pos_row + 1, pos_col}, 1)
+        Process.put({pos_row + 1, pos_col + 1}, "|")
+        Process.put({pos_row + 1, pos_col - 1}, "|")
+        beam({pos_row + 1, pos_col + 1})
+        beam({pos_row + 1, pos_col - 1})
     end
   end
 
   def part_a do
-    grid = File.stream!("input/7.txt")
+    File.stream!("input/7.txt")
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.codepoints/1)
     |> Stream.with_index
@@ -25,31 +25,31 @@ defmodule Solutions.Day07 do
       |> Stream.with_index
       |> Enum.map(fn {val, column} -> {{line, column}, val} end)
     end)
-    |> Enum.into(%{})
+    |> Enum.each(fn {key, val} -> Process.put(key, val) end)
 
-    start = Enum.find(grid, fn {_, val} -> val == "S" end) |> elem(0)
-    grid = beam(grid, start)
-    grid |> Map.values |> Enum.filter(&is_number/1) |> Enum.sum |> IO.puts
+    start = Process.get_keys("S") |> Enum.at(0)
+    beam(start)
+    Process.get |> Stream.map(&elem(&1, 1)) |> Enum.filter(&is_number/1) |> Enum.sum |> IO.puts
   end
 
-  def multi_beam(map, {pos_row, pos_col}) do
-    case map[{pos_row + 1, pos_col}] do
+  def multi_beam({pos_row, pos_col}) do
+    case Process.get({pos_row + 1, pos_col}) do
       "." ->
-        {map, newbeams} = multi_beam(map, {pos_row + 1, pos_col})
-        map = Map.put(map, {pos_row + 1, pos_col}, newbeams)
-        {map, newbeams}
-      nil -> {map, 1}
+        newbeams = multi_beam({pos_row + 1, pos_col})
+        Process.put({pos_row + 1, pos_col}, newbeams)
+        newbeams
+      nil -> 1
       "^" ->
-        {map, leftbeams} = multi_beam(map, {pos_row, pos_col - 1})
-        {map, rightbeams} = multi_beam(map, {pos_row, pos_col + 1})
-        {map, leftbeams + rightbeams}
+        leftbeams = multi_beam({pos_row, pos_col - 1})
+        rightbeams = multi_beam({pos_row, pos_col + 1})
+        leftbeams + rightbeams
       count when is_number(count) ->
-        {map, count}
+        count
     end
   end
 
   def part_b do
-    grid = File.stream!("input/7.txt")
+    File.stream!("input/7.txt")
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.codepoints/1)
     |> Stream.with_index
@@ -58,10 +58,10 @@ defmodule Solutions.Day07 do
       |> Stream.with_index
       |> Enum.map(fn {val, column} -> {{line, column}, val} end)
     end)
-    |> Enum.into(%{})
+    |> Enum.each(fn {key, val} -> Process.put(key, val) end)
 
-    start = Enum.find(grid, fn {_, val} -> val == "S" end) |> elem(0)
-    {_, count} = multi_beam(grid, start)
-    IO.puts count
+    Process.get_keys("S") |> Enum.at(0)
+    |> multi_beam
+    |> IO.puts
   end
 end
